f815201087efadb826f43fa73a3cbcfc41f7d2c8
Ayush Rao <ayushrao786420@gmail.com>
2024-08-21 14:58:13 +0530

    commit

--- src/nav.js
+++ src/nav.js
@@ -2,11 +2,14 @@ const express=require("express");
 const app=express();
 
 app.get("/",(req,res)=>{
-    res.send("<h3>HomePage</h3>");
+    res.send("<h3>Home-Page</h3>");
 });
 app.get("/about",(req,res)=>{
-    res.send("<h3>About Page</h3>");
+    res.send("<h3>About-Page</h3>");
 });
+app.get("/temp",(req,res)=>{
+    res.send("<h1>This is the temperature</h1>")
+})
 
 app.listen(8000,"127.0.0.1",()=>{
     console.log("Running on port 8000");
