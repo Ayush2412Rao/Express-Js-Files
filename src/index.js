7ebd12018302a11ba248afdee356f48c9971eaa7
Ayush Rao <ayushrao786420@gmail.com>
2024-08-25 04:02:12 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,22 +4,29 @@ const app = express();
 
 const staticPath = path.join(__dirname);
 
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
+app.set("view engine","hbs");
 
 // Routes
-app.get("/", (req, res) => {
-    //res.sendFile(express.static(staticPath,"index.html"));
-});
 
-app.get("/about", (req, res) => {
-    res.send("<h3>About-Page</h3>");
+app.get("/",(req,res)=>{
+    res.render("index",{
+        User:"Ayush",
+    });
 });
+// app.get("/", (req, res) => {
+//     //res.sendFile(express.static(staticPath,"index.html"));
+// });
 
-app.get("/temp", (req, res) => {
-    res.send("<h1>This is the temperature</h1>");
-});
+// app.get("/about", (req, res) => {
+//     res.send("<h3>About-Page</h3>");
+// });
+
+// app.get("/temp", (req, res) => {
+//     res.send("<h1>This is the temperature</h1>");
+// });
 
 // Start the server
-app.listen(8000, "127.0.0.1", () => {
-    console.log("Running on port 8000");
+app.listen(8080, "127.0.0.1", () => {
+    console.log("Running on port 8080");
 });
